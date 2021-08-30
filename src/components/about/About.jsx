66347fbc69071118a3909b3de7af54659502d1d2
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2021-08-30 16:57:05 -0300

    add responsive for pc screens

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,9 +19,9 @@ const About = () => {
     const [animationSkill, setAnimationSkills] = useState(false);
 
     const changeAnimationSkill = () => {
-        if(window.scrollY >= 810) {
+        if(window.scrollY >= 1000) {
             setAnimationSkills(true);
-        } else if(window.scrollY <= 850) {
+        } else if(window.scrollY <= 1200) {
             setAnimationSkills(false);
         }
     }
@@ -33,27 +33,29 @@ const About = () => {
         <>
         <div className="container-about">
             <div className="title-about">
-                <img src={about} alt="" />
                 <h1 id='title-h1' className={animation ? "animate__animated animate__zoomInUp" : "animate__animated animate__zoomOutDown"}>ABOUT ME</h1>
             </div>            
             <div className='me'>
-                <div className='me-image'><img src={Imagen} alt=''/></div>
-                <div className={animation ? "animate__animated animate__bounceInUp" : "animate__animated animate__backOutDown"}>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur tenetur laborum voluptas excepturi sed explicabo deleniti deserunt autem fuga, impedit magnam at molestias, illo rem ullam soluta velit ex.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum non recusandae deleniti distinctio quisquam illum perspiciatis adipisci tempora delectus illo obcaecati, ad minima id repudiandae reprehenderit repellendus saepe fugit rerum.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, voluptatem quis ab fugiat repellendus odit aut ipsum aliquid quos in ullam, incidunt cumque nisi eum similique, veritatis dicta commodi ex.</p>
+                <div className='container-me-image'>
+                    <img src={about} alt="" />
                 </div>
-                <div className="container-skills">
+                <div className='me-image'>
+                    <img src={Imagen} alt=''/>
+                    <div className={animation ? "animate__animated animate__bounceInUp" : "animate__animated animate__backOutDown"}>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat consequuntur tenetur laborum voluptas excepturi sed explicabo deleniti deserunt autem fuga, impedit magnam at molestias, illo rem ullam soluta velit ex.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum non recusandae deleniti distinctio quisquam illum perspiciatis adipisci tempora delectus illo obcaecati, ad minima id repudiandae reprehenderit repellendus saepe fugit rerum.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, voluptatem quis ab fugiat repellendus odit aut ipsum aliquid quos in ullam, incidunt cumque nisi eum similique, veritatis dicta commodi ex.</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className='container-skills'>
-        <div className="title-skills">
-            <h1 className={animationSkill ? "animate__animated animate__zoomInUp" : "animate__animated animate__zoomOutDown"}>SKILLS</h1>
-        </div>
-            <img src={Skills} id='img-skills' alt="" />
-        </div>
-
+            </div>
+            <div className='container-skills'>
+                <div className="title-skills">
+                    <h1 className={animationSkill ? "animate__animated animate__zoomInUp" : "animate__animated animate__zoomOutDown"}>SKILLS</h1>
+                </div>
+                <img src={Skills} id='img-skills' alt="" />
+            </div>
+            
         </>
     )
 }
